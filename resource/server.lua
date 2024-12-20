07bbaac8be1279ffc831149822a1ae8d5bc585dc
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2024-12-20 17:24:07 +0000

    Added distance checks

--- resource/server.lua
+++ resource/server.lua
@@ -1,8 +1,27 @@
 lib.locale()
 local config = lib.require('config')
 
+local model = 1395331371 -- prop_haybale_03
 
 lib.callback.register('s4t4n667_fibrepicking:PickFibre', function(source)
+    local nearBale = false
+    local ped = GetPlayerPed(source)
+    local coords = GetEntityCoords(ped)
+
+    local bale = GetClosestObjectOfType(coords.x, coords.y, coords.z, 5.0, model, false, false, false) -- 5.0 is the search radius
+
+    if bale ~= 0 then 
+        local objectCoords = GetEntityCoords(object) 
+        local dist = #(objectCoords - coords)  
+
+        if dist < config.target.distance then 
+            nearBale = true
+        end
+    end
+
+    if not nearBale then
+        return false
+    end
 
     local item = config.item
     local count = config.amountPicked
@@ -20,3 +39,4 @@ lib.callback.register('s4t4n667_fibrepicking:PickFibre', function(source)
         return false
     end
 end)
+
