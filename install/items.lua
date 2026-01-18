df8aee3e0788750ed36cbf7e54c1f478a3d94cdc
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2026-01-18 00:36:48 +0000

    Change item format

--- install/items.lua
+++ install/items.lua
@@ -1,2 +1,7 @@
-
-["fibres"] = { label = "Fibres", weight = 10, stack = true, close = false, description = "A handful of fibres.", },
+  ["fibres"] = { 
+    label = "Fibres", 
+    weight = 10, 
+    stack = true, 
+    close = false, 
+    description = "A handful of fibres.", 
+  },
