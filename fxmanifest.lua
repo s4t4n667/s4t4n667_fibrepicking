259c6fc2935c776d3b44b3b0834ba021c6d0b5f7
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2026-07-19 20:27:50 +0100

    Bump version from 2.0.2 to 2.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author "Asgaard Developments | s4t4n667"
 description 'Fibre picking resource, useful for crafting'
-version '2.0.2'
+version '2.0.3'
 
 shared_script {
     '@ox_lib/init.lua',
